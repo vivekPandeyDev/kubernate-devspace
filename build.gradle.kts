f88c9d9a1db6c649992b5341a23afc0d59124152
vivekPandeyDev <codewithvivekpandey@gmail.com>
2025-11-08 21:32:15 +0530

    feature: checkstyle,pmd,jacoco,spotbugs plugin added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.5.7"
 	id("io.spring.dependency-management") version "1.1.7"
+    id("checkstyle")
+    id("pmd")
+    id("jacoco")
+    id("com.github.spotbugs") version "5.0.13"
+    id("io.freefair.lombok") version "9.0.0"
 }
 
 group = "com.alex"
@@ -28,8 +34,6 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("io.github.wimdeblauwe:htmx-spring-boot:${findProperty("htmxSpringBootVersion")}")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
@@ -41,4 +45,58 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+checkstyle {
+    toolVersion = "10.12.0"
+    configFile = file("${project.rootDir}/config/checkstyle/checkstyle.xml")
+}
+
+tasks.register<Checkstyle>("checkstyleReport") {
+    group = "verification"
+    description = "Generates HTML Checkstyle report"
+    source = fileTree("src/main/java")
+    classpath = files()
+    configFile = file("${project.rootDir}/config/checkstyle/checkstyle.xml")
+    reports {
+        html.required.set(true)
+        html.outputLocation.set(file("${project.rootDir}/reports/checkstyle/checkstyle.html"))
+    }
+}
+
+
+pmd {
+    toolVersion = "7.18.0"
+    ruleSets = listOf("${project.rootDir}/config/pmd/pmd-ruleset.xml")
+    isIgnoreFailures = false
+}
+
+tasks.withType<Pmd> {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        html.outputLocation.set(file("${project.rootDir}/reports/pmd/${name}.html"))
+    }
+}
+
+
+jacoco {
+    toolVersion = "0.8.12"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+}
+
+spotbugs {
+    toolVersion.set("4.9.8")
+    ignoreFailures.set(false)
+    showProgress.set(true)
+    reportsDir.set(layout.buildDirectory.dir("spotbugsReports"))
+
 }
